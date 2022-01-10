44333af6861d821e239f0afa4191ed165d8b8c6e
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-01-10 23:23:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import colorCodeConversionTest
 
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+  colorCodeConversionTest.test_number_to_pair(4, 'White', 'Brown')
+  colorCodeConversionTest.test_number_to_pair(5, 'White', 'Slate')
+  colorCodeConversionTest.test_pair_to_number('Black', 'Orange', 12)
+  colorCodeConversionTest.test_pair_to_number('Violet', 'Slate', 25)
+  colorCodeConversionTest.test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
 
  
