01a4b983cd5a5dbff66ce24e06f211a1614dd499
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-01-12 23:39:06 +0530

    Update displayReferenceManual.py

--- displayReferenceManual.py
+++ displayReferenceManual.py
@@ -1,9 +1,13 @@
-majorColors = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+from initializations import ColorSets 
+
+ColorSet = ColorSets()
+
+MAJOR_COLORS = ColorSet.majorColors
+MINOR_COLORS = ColorSet.minorColors
 
 x = 1
 print (f"{'Pair Number' : ^10} {'Major Color' : ^15} {'Minor Color' : ^15}");
-for y in majorColors:
-    for z in minorColors:
+for y in MAJOR_COLORS:
+    for z in MINOR_COLORS:
         print (f"{str(x) : ^10} {y : ^15} {z : ^15}");
         x+=1
