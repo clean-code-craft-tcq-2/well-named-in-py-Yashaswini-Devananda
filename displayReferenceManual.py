bd77c092068f9362c49bd8dbf97b556cc77ed1e2
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-01-12 23:50:26 +0530

    Update displayReferenceManual.py

--- displayReferenceManual.py
+++ displayReferenceManual.py
@@ -6,9 +6,9 @@ MAJOR_COLORS = ColorSet.majorColors
 MINOR_COLORS = ColorSet.minorColors
 
 def reference_manual():
-    x = 1
+    pairNum = 1
     print (f"{'Pair Number' : ^10} {'Major Color' : ^15} {'Minor Color' : ^15}");
-    for y in MAJOR_COLORS:
-        for z in MINOR_COLORS:
-            print (f"{str(x) : ^10} {y : ^15} {z : ^15}");
-            x+=1
+    for majorColor in MAJOR_COLORS:
+        for minorColor in MINOR_COLORS:
+            print (f"{str(pairNum) : ^10} {majorColor : ^15} {minorColor : ^15}");
+            pairNum+=1
